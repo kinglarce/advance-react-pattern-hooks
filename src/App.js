7b1eff9733c92e5726c666fd047a2a72b661e09b
King Larce Cortes <larce@Kings-MacBook-Pro.local>
2019-07-28 14:05:39 +0900

    custom hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import Expandable from './components/Expandable'
+import useExpanded from './useExpanded.js'
+import useEffectAfterMount from './useEffectAfterMount.js'
 
 const doSomethingPersonal = () => {
   console.log('somethins expanded');
@@ -22,24 +24,43 @@ const information = [
 ]
 
 
-function App() {
-  const [activeIndex, setActiveIndex] = useState(null)
-  const onExpand = evt => setActiveIndex(evt.target.dataset.index)
+// function App() {
+//   const [activeIndex, setActiveIndex] = useState(null)
+//   const onExpand = evt => setActiveIndex(evt.target.dataset.index)
 
+//   return (
+//     <div className="App">
+//       {information.map(({ header, note }, index) => (
+//         // the "+" converts the `activeIndex` to a number
+//         <Expandable key={index} shouldExpand={index === +activeIndex} onExpand={onExpand}>
+//           <Expandable.Header data-index={index}>
+//             {header}
+//           </Expandable.Header>
+//           <Expandable.Icon />
+//           <Expandable.Body>{note}</Expandable.Body>
+//         </Expandable>
+//       ))}
+//     </div>
+//   );
+// }
+
+function App () {
+  const { expanded, toggle } = useExpanded()	
+  // look here 👇
+  useEffectAfterMount(
+    () => {
+      // user can perform any side effect here 👇
+      console.log('Yay! button was clicked!!')
+    },
+    [expanded]
+  )
   return (
-    <div className="App">
-      {information.map(({ header, note }, index) => (
-        // the "+" converts the `activeIndex` to a number
-        <Expandable key={index} shouldExpand={index === +activeIndex} onExpand={onExpand}>
-          <Expandable.Header data-index={index}>
-            {header}
-          </Expandable.Header>
-          <Expandable.Icon />
-          <Expandable.Body>{note}</Expandable.Body>
-        </Expandable>
-      ))}
+    <div style={{ marginTop: '3rem' }}>
+      <button onClick={toggle}>Click to view awesomeness...</button>
+      {expanded ? <p>{'😎'.repeat(50)}</p> : null}
     </div>
-  );
+  )
 }
+
 
 export default App;
---
+++ src/useEffectAfterMount.js
@@ -0,0 +1,12 @@
+// useEffectAfterMount.js
+import { useRef, useEffect } from 'react'
+
+export default function useEffectAfterMount (cb, deps) {
+  const componentJustMounted = useRef(true)
+  useEffect(() => {
+    if (!componentJustMounted.current) {
+      return cb()
+    }
+    componentJustMounted.current = false
+  }, deps)
+}
